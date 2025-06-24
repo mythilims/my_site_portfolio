7f9aab7666da0baf27339476af4e521b565b68ae
mythili <mythilijobofficial@gmail.com>
2025-06-24 18:04:39 +0530

    4th commit

--- src/pages/Myportfolio.jsx
+++ src/pages/Myportfolio.jsx
@@ -83,7 +83,7 @@ function Myportfolio() {
      
 
       <section className="bg-[#0b1120] text-white " id="home">
-        <div className="max-w-5xl mx-auto space-y-10  flex flex-col md:flex-row items-center pt-30 px-6 md:px-12 ">
+        <div  className="max-w-5xl mx-auto space-y-10  flex flex-col md:flex-row items-center pt-30 px-6 md:px-12 ">
           <div className="md:w-1/2 ">
             <h2 className="text-3xl font-bold mb-4">
               {HELLO_TEXT}{" "}
